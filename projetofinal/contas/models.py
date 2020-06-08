aec0f23b179f2676ec125def5bfd6eff885152d9
HugoVinicius <hugo.desenvolvimento@outlook.com>
2020-06-08 01:43:22 -0300

    Em andamento

--- projetofinal/contas/models.py
+++ projetofinal/contas/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class ContasPagar(models.Model):
+    nome = models.CharField(max_length=200)
+    valor = models.FloatField()
+    data_vencimento = models.DateField(null = True)
+    data_pagamento = models.DateField(null = True)
+
+    CP_CLASSIFICACAO_CHOICES = [
+        ('1', 'Telecomunicações'),
+        ('2', 'Água'),
+        ('3', 'Energia'),
+        ('4', 'Alimentação'),
+        ('5', 'Outros'),
+    ]
+
+    classificacao = models.CharField(
+        max_length=4,
+        choices=CP_CLASSIFICACAO_CHOICES,
+    )
+
+    CP_FORMA_PAGAMENTO_CHOICES = [
+        ('1', 'Boleto'),
+        ('2', 'Crédito'),
+        ('3', 'Débito'),
+        ('4', 'Depósito'),
+        ('5', 'Outros'),
+    ]
+
+    forma_pagamento = models.CharField(
+        max_length=4,
+        choices=CP_FORMA_PAGAMENTO_CHOICES,
+    )
+
+    CP_SITUACAO_CHOICES = [
+        ('1', 'Pago'),
+        ('2', 'A pagar'),
+    ]
+    
+    situacao = models.CharField(
+        max_length=4,
+        choices=CP_SITUACAO_CHOICES,
+    )
+    dt_criacao = models.DateTimeField(auto_now_add=True)
