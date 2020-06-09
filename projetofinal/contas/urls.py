8698298bee3cdb5fd22d41f067c69cb1575b9417
HugoVinicius <hugo.desenvolvimento@outlook.com>
2020-06-09 00:56:39 -0300

    Criando Relatório e Fluxo de Caixa

--- projetofinal/contas/urls.py
+++ projetofinal/contas/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import home as hv
 from .views import contas_pagar as cpv
 from .views import contas_receber as crv
+from .views import relatorio as rv
+from .views import fluxo_caixa as fv
 
 urlpatterns = [
 	path('', hv.home, name="home"),
@@ -14,4 +16,6 @@ urlpatterns = [
     path('recebimento/cadastrar', crv.cadastrar, name="cadastrar_recebimento"),
 	path('recebimento/atualizar/<int:pk>/', crv.atualizar, name="atualizar_recebimento"),
 	path('recebimento/excluir/<int:pk>/', crv.excluir, name="excluir_recebimento"),	
+	path('relatorio/', rv.relatorio, name="relatorio"),	
+	path('fluxocaixa/', fv.fluxocaixa, name="fluxo_caixa"),	
 ]
---
+++ projetofinal/contas/views/fluxo_caixa.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
+
+from ..models import ContasPagar
+
+@csrf_exempt
+@require_http_methods(["GET"])
+def fluxocaixa(request):
+    data = {}
+    data['listaContas'] = ContasPagar.objects.all()
+
+    return render(request, 'contas/fluxocaixa.html', data)
